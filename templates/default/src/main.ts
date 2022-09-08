c9789e6c6e79dc6873d8466407659c552c85c20b
Sam Gaus <sam@gaus.co.uk>
2022-09-08 17:16:08 +0100

    Move to export default since this should work in r26731

--- templates/default/src/main.ts
+++ templates/default/src/main.ts
@@ -1,6 +1,3 @@
-function main(): void {
+export default function main(): void {
   console.log("You have successfully built {{name}}!");
 }
-
-// This special export lets KoLmafia know where to start running your script from
-module.exports.main = main;
