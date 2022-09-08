2da3f76ee2b9d2aec968d7ba4ed78b4b65c98283
Sam Gaus <sam@gaus.co.uk>
2022-09-08 11:43:48 +0100

    Ask if libram should be installed, add comment to main.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import { AfterHookOptions, create } from 'create-create-app';
-import { resolve } from 'path';
+import { AfterHookOptions, create } from "create-create-app";
+import { resolve } from "path";
 import chalk from "chalk";
 
-const templateRoot = resolve(__dirname, '..', 'templates');
+const templateRoot = resolve(__dirname, "..", "templates");
 
 function caveat({ answers }: AfterHookOptions) {
   const pm = answers.packageManager || "npm";
@@ -15,12 +15,29 @@ Once you've navigated to the directory you can make changes to \`main.ts\` and r
 }
 
 function main() {
-  console.log(`🍸📜 Welcome to ${chalk.bold("create-kolmafia-script")}! We're going to ask a few questions to get you started`);
+  console.log(
+    `🍸📜 Welcome to ${chalk.bold(
+      "create-kolmafia-script"
+    )}! We're going to ask a few questions to get you started`
+  );
 
-  create('create-kolmafia-script', {
+  create("create-kolmafia-script", {
     templateRoot,
     promptForLicense: false,
-    after: ({ answers }) => console.log(`Ok you chose ${answers.architecture}.`),
+    extra: {
+      libram: {
+        type: "confirm",
+        describe: `Would you like to install ${chalk.italic(
+          "libram"
+        )} as a dependency?`,
+        default: true,
+      },
+    },
+    after: ({ answers, installNpmPackage }) => {
+      if (answers.libram) {
+        installNpmPackage("libram");
+      }
+    },
     caveat,
   });
 }
--- templates/default/src/main.ts
+++ templates/default/src/main.ts
@@ -2,4 +2,5 @@ function main(): void {
   console.log("You have successfully built {{name}}!");
 }
 
+// Export the main function so that KoLmafia can find it
 module.exports.main = main;
